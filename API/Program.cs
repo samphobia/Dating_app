8c5797bdf7e7a8072022210bca78cf5afcb1c61d
Samuel Osinubi <samobafemisayo@gmail.com>
2023-07-20 14:13:44 +0100

    adding custom errors route BE

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +18,8 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 if (app.Environment.IsDevelopment())
