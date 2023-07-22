7b27af977140b81175a7aa8172d0571dda1fce59
Samuel Osinubi <samobafemisayo@gmail.com>
2023-07-22 16:49:37 +0100

    Installed and implemented autoMapper to map out dats from Dto

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -9,14 +9,15 @@ namespace API.Extensions
   {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services,
         IConfiguration config)
-        {
-        services.AddDbContext<DataContext>(opt =>
-        {
-            opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
-        });
-        services.AddCors();
-        services.AddScoped<ITokenService, TokenService>();
-        services.AddScoped<IUserRepository, UserRepository>();
+    {
+      services.AddDbContext<DataContext>(opt =>
+      {
+        opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+      });
+      services.AddCors();
+      services.AddScoped<ITokenService, TokenService>();
+      services.AddScoped<IUserRepository, UserRepository>();
+      services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
       return services;
     }
